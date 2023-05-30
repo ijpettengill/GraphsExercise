a3a46ab7efa871a11831bc744a3d38975b452096
Issaac Pettengill <issaac@Issaacs-iMac.lan>
2023-05-30 15:48:02 -0400

    final drafts

---
+++ graphs.js
@@ -0,0 +1,82 @@
+class Node {
+    constructor(value, adjacent = new Set()) {
+      this.value = value;
+      this.adjacent = adjacent;
+    }
+  }
+  
+  class Graph {
+    constructor() {
+      this.nodes = new Set();
+    }
+  
+    // this function accepts a Node instance and adds it to the nodes property on the graph
+    addVertex(vertex) {
+      if (vertex instanceof Node) {
+        this.nodes.add(vertex);
+      }
+    }
+  
+    // this function accepts an array of Node instances and adds them to the nodes property on the graph
+    addVertices(vertexArray) {
+      for (let vertex of vertexArray) {
+        this.nodes.add(vertex);
+      }
+    }
+  
+    // this function accepts two vertices and updates their adjacent values to include the other vertex
+    addEdge(v1, v2) {
+      v1.adjacent.add(v2);
+      v2.adjacent.add(v1);
+    }
+  
+    // this function accepts two vertices and updates their adjacent values to remove the other vertex
+    removeEdge(v1, v2) {
+      v1.adjacent.delete(v2);
+      v2.adjacent.delete(v1);
+    }
+  
+    // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
+    removeVertex(vertex) {
+      this.nodes.delete(vertex);
+      for (let node of this.nodes) {
+        node.adjacent.delete(vertex);
+      }
+    }
+  
+    // this function returns an array of Node values using DFS
+    depthFirstSearch(start, visited=new Set([start])) {
+      const nodes = [start.value];
+  
+      for (let child of start.adjacent) {
+        if (!visited.has(child)) {
+          visited.add(child);
+          nodes.push(...this.depthFirstSearch(child, visited));
+        }
+      }
+      
+      return nodes;
+    }
+  
+    // this function returns an array of Node values using BFS
+    breadthFirstSearch(start) {
+      const visited = new Set();
+      const toVisit = [start];
+  
+      visited.add(start);
+      while (toVisit.length) {
+        const current = toVisit.shift();
+        
+        for (let node of current.adjacent) {
+          if (!visited.has(node)) {
+            toVisit.push(node);
+          }
+        }
+        visited.add(current);
+      }
+  
+      return Array.from(visited).map(node => node.value);
+    }
+  }
+  
+  module.exports = {Graph, Node}
